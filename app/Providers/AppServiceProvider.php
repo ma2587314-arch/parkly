0e8e8987a344b9c394988efe1cb183a6fb3600a1
ma2587314-arch <ma2587314@gmail.com>
2026-06-07 19:18:04 +0300

    fix: resolve 419 CSRF issue on Railway FrankenPHP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Force HTTPS for all generated URLs (fixes Mixed Content / HTTP form action)
         \Illuminate\Support\Facades\URL::forceScheme('https');
     }
 }
